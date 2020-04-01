42a7e3a2d99bacf0213d4f1225283db68af0b105
Megan Horsfield <meganthadev@gmail.com>
2020-04-01 03:48:53 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,14 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   
-  
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
   def up
   end
  
   def down
   end
+  
 end
